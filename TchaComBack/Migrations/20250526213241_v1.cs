a5656c195144da3abba55732a707b0090c45fa7d
joaomedrado <124898689+joaomedrado@users.noreply.github.com>
2025-05-26 19:45:26 -0400

    att

---
+++ TchaComBack/Migrations/20250526213241_v1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TchaComBack.Migrations
+{
+    /// <inheritdoc />
+    public partial class v1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "HorasTrabalhadas",
+                table: "ExtratoPonto",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HorasPositivas",
+                table: "ExtratoPonto",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HorasNegativas",
+                table: "ExtratoPonto",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HorasExtras",
+                table: "ExtratoPonto",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HorasACumprir",
+                table: "ExtratoPonto",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HoraSaida2",
+                table: "ExtratoPonto",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HoraSaida1",
+                table: "ExtratoPonto",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HoraEntrada2",
+                table: "ExtratoPonto",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HoraEntrada1",
+                table: "ExtratoPonto",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "HorasTrabalhadas",
+                table: "ExtratoPonto",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "HorasPositivas",
+                table: "ExtratoPonto",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "HorasNegativas",
+                table: "ExtratoPonto",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "HorasExtras",
+                table: "ExtratoPonto",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "HorasACumprir",
+                table: "ExtratoPonto",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "HoraSaida2",
+                table: "ExtratoPonto",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "HoraSaida1",
+                table: "ExtratoPonto",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "HoraEntrada2",
+                table: "ExtratoPonto",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "HoraEntrada1",
+                table: "ExtratoPonto",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
